159da972dc95161b55f9de17e5356b4f5c81c83e
Mav Sanchez <maverickssanchez@gmail.com>
2020-04-04 22:51:34 -0700

    Updated map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,7 @@
+let yearButton08 = d3.select("#results08");
+let yearButton12 = d3.select("#results12");
+let yearButton16 = d3.select("#results16");
+
 // Creating map object
 
 let us_latlng = {
@@ -21,88 +25,87 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(myMap);
 
 // Load in GeoJson data
-const geoData = "https://raw.githubusercontent.com/mavsanchez/united-states-elections-project2/master/static/data/processed.json";
+// const geoData = "https://raw.githubusercontent.com/mavsanchez/united-states-elections-project2/master/static/data/processed.json";
+const geoData = "static/data/processed.json";
 
 fill_opacity = d3.scaleLinear()
-  .domain([0, 100]) 
+  .domain([0, 100])
   .range([.4, 1])
+let geojsonLayer = null;
 
-let geojson;
-
-// Grab data with d3
-d3.json(geoData, function (response){
-  console.log(response);
-  let colorUse;
-  // Create a new choropleth layer
-  geojson = L.choropleth(response,{
-    // Define what  property in the features to use
-    // valueProperty: 'percentwon_dem',
+function updateMap(event){
+    let selected_year = d3.event.target.value || 2016;
     
-    // valueProperty: function (feature) { colorUse = feature.properties.winner; console.log('one',colorUse);  return (feature.properties.winner == "blue") ? feature.properties.percentwon_dem : feature.properties.percentwon_rep; },//'percentwon_dem',
-    // Set color scale
-    // scale: function (feature) { return (feature.properties.winner == "blue") ? new Array("white", "blue") : new Array("white", "red"); },
-    // scale: ["white", "red"],
-    // Number of breaks in step range
-    // steps: 100,
-    // q for quartile, e for equidistant, k for k-means
-    // mode: 'q',
-    // style: {
-    //   color: '#fff',
-    //   weight: 1,
-    //   fillOpacity: 0.5
-    // },
-    style: function (feature) {
-      switch (feature.properties.winner) {
-        case "red":
-          return {
-            fillColor: "#800000", 
-            fillOpacity:  fill_opacity(Math.abs(feature.properties.percentwon_rep-feature.properties.percentwon_dem)),  
-            color: 'white',
-            weight: 0.5
+      // Grab data with d3
+      d3.json(geoData, function (response) {
+        // For initial clearing
+        if (geojsonLayer !== null) { myMap.removeLayer(geojsonLayer); }
+
+        // Create a new choropleth layer
+        geojsonLayer = L.choropleth(response, {
+          filter: function (feature) {
+            if (feature.properties.year === parseInt(selected_year)){ return true; }
+          },
+          style: function (feature) {
+            switch (feature.properties.winner) {
+              case "red":
+                return {
+                  fillColor: "#800000",
+                  fillOpacity: fill_opacity(Math.abs(feature.properties.percentwon_rep - feature.properties.percentwon_dem)),
+                  color: 'white',
+                  weight: 0.5
+                }
+              default:
+                return {
+                  fillColor: "#000080",
+                  fillOpacity: fill_opacity(Math.abs(feature.properties.percentwon_rep - feature.properties.percentwon_dem)),
+                  color: 'white',
+                  weight: 0.5
+                }
+            }
+          },
+          // Binding a pop-up to each layer
+          onEachFeature: function (feature, layer) {
+            layer.on({
+            mouseover: function (event) {
+                layer = event.target;
+                layer.setStyle({
+                  color: 'yellow',
+                  weight: 2
+                });
+              },
+              mouseout: function (event) {
+                layer = event.target;
+                layer.setStyle({
+                  color: 'white',
+                  weight: 0.5
+                });
+              }
+            });
+
+          layer.bindTooltip(
+            `<h6>${feature.properties.county}, ${feature.properties.state_po} (${feature.properties.year}) </h6>
+            <table class="table mb-0">
+            <thead>
+            <tr><th>Candidate</th><th>Party</th><th>Won</th></tr>
+            </thead>
+            <tbody>
+            <tr><td>${feature.properties.candidate_dem}</td> <td>${feature.properties.party_dem}</td> <td>${feature.properties.percentwon_dem || 0}%</td></tr>
+            <tr><td>${feature.properties.candidate_rep}</td> <td>${feature.properties.party_rep}</td> <td>${feature.properties.percentwon_rep || 0}%</td></tr>
+            </tbody>
+            </table>
+            Total votes ${feature.properties.totalvotes_dem.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")} 
+            with ${Math.abs(feature.properties.percentwon_rep - feature.properties.percentwon_dem).toFixed(2)}%  margin
+            `
+            )
           }
-        default:
-          return {
-            fillColor: "#000080",
-            fillOpacity:  fill_opacity(Math.abs(feature.properties.percentwon_rep-feature.properties.percentwon_dem)),  
-            color: 'white',
-            weight: 0.5
-          }
+        }).addTo(myMap);
       }
-      },
-    // Binding a pop-up to each layer
-    onEachFeature: function (feature, layer) {
-      layer.on({
-        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-        mouseover: function (event) {
-          layer = event.target;
-          layer.setStyle({
-            color: 'yellow',
-            weight: 2
-          });
-        },
-        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-        mouseout: function (event) {
-          layer = event.target;
-          layer.setStyle({
-            color: 'white',
-            weight: 0.5
-          });
-        }
-      });
-
-      // properties = ['year', 'state_po', 'county', 'candidate', 'party', 'candidatevotes', 'totalvotes', 'percentwon']
-      // layer.bindPopup(`<h6>${feature.properties.county}</h6>
-      //                  <table class="table"> <tr> <th scope="col">Candidate</th> <th scope="col">Party</th> <th scope="col">Won</th> </tr> <tr> <th scope="row">${feature.properties.candidate_dem || 0}</th> <td>${feature.properties.party_dem || 0}</td> <td>${feature.properties.percentwon_dem || 0}%</td> </tr><tr> <th scope="row">${feature.properties.candidate_rep || 0}</th> <td>${feature.properties.party_rep || 0}</td> <td>${feature.properties.percentwon_rep || 0}%</td> </tr> </table>`
-      //                  )
-      // Removed the line below for faster rendering. 
-      //  <table class="table table-condensed mb-0 pb-0"><tr> <th>Candidate</th> <th>Party</th> <th>Won</th> </tr> <tr> <td scope="row">${feature.properties.candidate_dem || 0}</td> <td>${feature.properties.party_dem || 0}</td> <td>${feature.properties.percentwon_dem || 0}%</td> </tr><tr> <td scope="row">${feature.properties.candidate_rep || 0}</td> <td>${feature.properties.party_rep || 0}</td> <td>${feature.properties.percentwon_rep || 0}%</td></tr></table>`
-      layer.bindTooltip(`<h6>${feature.properties.county}, ${feature.properties.state_po} </h6>
-                          Total votes: ${feature.properties.totalvotes_dem.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
-                       <table class="table table-condensed mb-0 pb-0"><tr> <th>Candidate</th> <th>Party</th> <th>Won</th> </tr> <tr> <td scope="row">Hillary Clinton</td> <td>Democrat</td> <td>${feature.properties.percentwon_dem || 0}%</td> </tr><tr> <td scope="row">Donald Trump</td> <td>Republican</td> <td>${feature.properties.percentwon_rep || 0}%</td></tr></table>`
-                       )
-    }     
-  }).addTo(myMap);
+      );
+    } //main function
 
 
-
-});
+yearButton08.on('click', updateMap);
+yearButton12.on('click', updateMap);
+yearButton16.on('click', updateMap);
+d3.select(window).on("load", updateMap); //defaults to 2016
